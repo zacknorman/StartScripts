33d1b8550ec053079a58ced836551ac112abbcd6
Zack Norman <zackee14@gmail.com>
2016-01-30 11:02:08 -0500

    Update .bashrcHelper.sh

--- .bashrcHelper.sh
+++ .bashrcHelper.sh
@@ -1,4 +1,32 @@
 #WELCOME TO THE BASH SCRIPT HELPER
 ##THIS IS THE ASCII ART DECLARATION SECTION
-a = "
-
+a="
+  __ _ 
+ / _` |
+| (_| |
+ \__,_|"
+b=" 
+ _     
+| |    
+| |__  
+| '_ \ 
+| |_) |
+|_.__/ 
+       "
+c="      
+  ___ 
+ / __|
+| (__ 
+ \___|
+"
+d="   
+     _ 
+    | |
+  __| |
+ / _` |
+| (_| |
+ \__,_|
+       "
+  #CALL
+name = input("Enter Your Desired Name: ")
+print("hello, " name)
